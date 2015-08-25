1e851236b3f5724443e27b99152da6eb60f41ede
Your Name <you@example.com>
2015-08-25 21:29:40 +0600

    init

---
+++ zombies2-html/src/com/zombie/client/GwtLauncher.java
@@ -0,0 +1,19 @@
+package com.zombie.client;
+
+import com.zombie.ZombieGame;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+public class GwtLauncher extends GwtApplication {
+	@Override
+	public GwtApplicationConfiguration getConfig () {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
+		return cfg;
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener () {
+		return new ZombieGame();
+	}
+}
